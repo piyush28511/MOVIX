629bd49c9cb2bfda1e886918a78380a4b993f481
Piyush Maurya <piyush@Piyushs-MacBook-Air-3.local>
2023-11-30 15:11:51 +0530

    error fixed

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import "./style.scss"
+import "./style.scss";
 
-const pageNotFound = () => {
+import ContentWrapper from "../../component/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
     return (
-        <div>pageNotFound</div>
-    )
-}
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-export default pageNotFound
+export default PageNotFound;
